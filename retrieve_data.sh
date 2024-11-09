1c75c8f628973eb6b881646eed0def6a5ae6113d
Oluwakemi Akinsiku <akinsikukemisola@gmail.com>
2024-11-09 14:17:56 +0100

    Added data for version 1.1

--- retrieve_data.sh
+++ retrieve_data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Load the version and size from params.yml
-version=$(grep -m1 "version:" params.yml | awk '{print $2}' | tr -d '"')
-size=$(grep -A1 "size:" params.yml | grep "$version" | awk '{print $2}' | tr -d '"')
+# Load the version from params.yml
+version=$(yq e '.version' params.yml)
+
+# Load the corresponding size for the version from params.yml
+size=$(yq e ".size.\"$version\"" params.yml)
 
 # Debugging outputs
 echo "Version: $version"
@@ -13,12 +15,12 @@ API_URL="https://jsonplaceholder.typicode.com/photos"
 
 # Fetch data from the API, limiting to the specified size
 echo "Fetching data for version $version with expected size of $size rows..."
-curl -s "$API_URL" | jq ".[:${size}]" > datahub/data.json
+curl -s "$API_URL" | jq ".[:$size]" > datahub/old_data.json
 
 # Check if data.json has changed
-if cmp -s datahub/data.json datahub/old_data.json; then
+if cmp -s datahub/old_data.json datahub/data.json; then
     echo "No changes; data has not changed."
 else
     echo "Data has been updated."
-    mv datahub/data.json datahub/old_data.json
+    mv datahub/old_data.json datahub/data.json
 fi
